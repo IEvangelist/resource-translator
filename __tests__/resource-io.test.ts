71d416dbd2879d193eabd962639c36e6884e6ca8
IEvangelist <david.pine.7@gmail.com>
2020-07-20 07:15:54 -0500

    More stuff...

--- __tests__/resource-io.test.ts
+++ __tests__/resource-io.test.ts
@@ -17,7 +17,12 @@ test("read file correctly parses known XML", async () => {
     const resourcePath = resolve(__dirname, "./data/Test.en.resx");
     const resourceXml = await readFile(resourcePath);
     
+    console.error(JSON.stringify(resourceXml));
+
     expect(resourceXml).toBeTruthy();
+    expect(resourceXml.root).toBeTruthy();
+    expect(resourceXml.root.data).toBeTruthy();
+    expect(resourceXml.root.data.length).toBeTruthy();
     expect(resourceXml.root.data[0].$.name).toEqual("Greeting");
     expect(resourceXml.root.data[0].value[0]).toEqual("Hello world, this is a test.... only a test!");
 });
